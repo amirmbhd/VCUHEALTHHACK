8ce2d8fc9e32fd1c9eea8c8e1ff894254eb21d67
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2021-11-06 22:50:18 -0400

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -137,18 +137,18 @@ def main():
                     elif activity == "Medications":
                         st.subheader("Medications")
                         st.markdown("**Insulin Glargine [Lantus SoloStar] 100 mg/mL for Type 2 Diabetes**")
-                        st.markdown(" **Direction:** Inject ten (10) units into the side of your lower abdomen once a day. Injection instructions are included with the medication pen.")
-                        st.markdown(" **Storage:** Refrigerate medication pens until ready to inject. Take one pen out of the fridge and let sit for 5 minutes before injection. Do not store medication pen outside of the refrigerator")
-                        st.markdown(" **Side effects:** Sudden and violent death (70% of cases), mild headache (3%).")
-                        st.markdown(" **Amlodipine 5mg for High Blood Pressure**")
-                        st.markdown(" **Direction:** Take one (1) tablet by mouth once a day for hypertension."
-                        st.markdown(" **Storage:** Store at room temperature away from moisture or direct sunlight.")
-                        st.markdown(" **Side effects:** Undesired stroke event (50% of cases).")
-                        st.markdown(" **My Pharmacy Contacts:**")
-                        st.markdown(" **Pharmacist’s Name:** Dr. Uwu Lepolski")
-                        st.markdown(" **Address:** 4448 West Coast Dr., Richmond, VA 23220")
-                        st.markdown(" **Phone Number:** (804) 987-6555")
-                        st.markdown(" Call the number if you have questions about your current medications")
+                        st.markdown("**Direction:** Inject ten (10) units into the side of your lower abdomen once a day. Injection instructions are included with the medication pen.")
+                        st.markdown("**Storage:** Refrigerate medication pens until ready to inject. Take one pen out of the fridge and let sit for 5 minutes before injection. Do not store medication pen outside of the refrigerator")
+                        st.markdown("**Side effects:** Sudden and violent death (70% of cases), mild headache (3%).")
+                        st.markdown("**Amlodipine 5mg for High Blood Pressure**")
+                        st.markdown("**Direction:** Take one (1) tablet by mouth once a day for hypertension."
+                        st.markdown("**Storage:** Store at room temperature away from moisture or direct sunlight.")
+                        st.markdown("**Side effects:** Undesired stroke event (50% of cases).")
+                        st.markdown("**My Pharmacy Contacts:**")
+                        st.markdown("**Pharmacist’s Name:** Dr. Uwu Lepolski")
+                        st.markdown("**Address:** 4448 West Coast Dr., Richmond, VA 23220")
+                        st.markdown("**Phone Number:** (804) 987-6555")
+                        st.markdown("Call the number if you have questions about your current medications")
                    
 			
 			
