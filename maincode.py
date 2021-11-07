0a69ec98ae920d323a6e7d752bdb940d4e0e28b4
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2021-11-07 09:15:51 -0500

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -47,7 +47,7 @@ def load_images(image_name):
     return st.image(img, width=600)
 def load_images2(image_name):
     img = Image.open(image_name)
-    return st.image(img2, width=400)
+    return st.image(img, width=400)
 
 feature_names_best = [ 'fasting_bg', 'Gender', 'Polyuria', 'Polydipsia', 'Sudden_weight_loss', 'Weakness', 'Polyphagia', 'Genital_thrush', 'Visual_blurring','Itching', 'Irritability', 'Delayed_healing', 'Partial_paresis','Muscle_stiffness', 'Alopecia', 'Obesity']
 
