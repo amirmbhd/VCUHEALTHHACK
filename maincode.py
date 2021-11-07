47b4766e2dc107c5a2e4f94361ba94a6b9209162
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2021-11-06 21:02:11 -0400

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -112,7 +112,6 @@ def main():
                         st.markdown(" If you have any questions, or if you need to reschedule your next appointment, please call me using the phone number above!")
                     elif activity == ( "Visit Summary"): 
                         st.subheader("Visit Summary")
-                        st.subheader("Visit Summary")
                         st.markdown("Date: 10/10/2021")
                         st.markdown(" **Doctor’s Notes:**")
                         st.markdown(" Patient presented for a routine checkup. Vitals are all within range considering diabetes mellitus. Continue medications, exercise routine, and diet. Weight loss is at a steady and healthy pace. There were no observations of side effects from medications.")
