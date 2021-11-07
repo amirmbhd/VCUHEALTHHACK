b8736a146f7bc2e20ef05302c12cb2cf2c8c56f9
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2021-11-06 20:32:05 -0400

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -105,7 +105,7 @@ def main():
                         st.markdown(" No change in exercise recommendations.")
                         st.markdown("** My next appointment **")
                         st.markdown(" General Checkup on 12/10/2022 at 10:00 am. Make sure not to eat anything that morning. You can drink water.")	
-                        st.markdown("** My Doctor’s Contact Information")
+                        st.markdown("** My Doctor’s Contact Information **")
                         st.markdown(" Address: 4444 West Coast Dr., Richmond, VA 23220")
                         st.markdown(" Phone Number: (804) 987-6554")
                         st.markdown(" Office Hours: 8 am to 3 pm, Monday to Thursday")
