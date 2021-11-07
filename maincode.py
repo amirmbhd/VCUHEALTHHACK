b4203a83dffde90885c1315390b681e4f4f976a5
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2021-11-06 21:28:52 -0400

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -125,11 +125,11 @@ def main():
                         st.markdown(" **Lab Results**")
                         st.markdown(" Lab results are all normal. ")
                         def show_pdf(file_path):
-				with open(SampleVisitSummary.pdf,"rb") as f:
-				base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-			pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-			st.markdown(pdf_display, unsafe_allow_html=True)
-			show_pdf("my_pdf.pdf")
+						with open(SampleVisitSummary.pdf,"rb") as f:
+							base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+						pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+						st.markdown(pdf_display, unsafe_allow_html=True)
+						show_pdf("my_pdf.pdf")
 
 
 			
