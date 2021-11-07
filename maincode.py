b77cebc3be8cfbacc9c098dd9809fb4d7945b279
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2021-11-06 21:27:46 -0400

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -129,9 +129,7 @@ def main():
 				base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 			pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
 			st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-show_pdf("my_pdf.pdf")
+			show_pdf("my_pdf.pdf")
 
 
 			
