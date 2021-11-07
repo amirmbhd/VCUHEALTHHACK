281d6263429aadc0924985a79170e977be63755b
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2021-11-06 22:04:21 -0400

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -124,7 +124,7 @@ def main():
                         st.write("check out this [link](https://drive.google.com/file/d/1eRJHrcukUcoKUsmx0htwtJDTOdaMsA7m/view?usp=sharing)")
                         st.markdown(" **Lab Results**")
                         st.markdown(" Lab results are all normal. ")
-			SampleVisitSummary.pdf = spf
+                        SampleVisitSummary.pdf = spf
                         st.markdown(spf , unsafe_allow_html=True)
                        
 
