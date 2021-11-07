94e451f2b85cbb683d896b5a6d1fcab861ed0f12
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2021-11-06 23:24:58 -0400

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -25,12 +25,7 @@ from PIL import Image
 
 from managedb import *
 
-[theme]
-primaryColor="#F63366"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="sans serif"
+
 
 # Password 
 def generate_hashes(password):
